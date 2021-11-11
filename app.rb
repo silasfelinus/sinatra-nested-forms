de9ab23eac72462087fe849e5494ede03dc2a1d8
silasfelinus <silasfelinus@gmail.com>
2021-11-10 20:18:29 -0800

    commit tree 553c6b2a29948b93c2aa7d2cb03429571e5ebcf8

--- app.rb
+++ app.rb
@@ -3,7 +3,25 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
-
+  get '/' do
+    erb :root
   end
+
+  get '/new' do
+    erb :new
+  end
+
+  post '/pirates' do
+    @pirate = Pirate.new(params[:pirate])
+    params[:pirate][:ships].each do |details|
+      Ship.new(details)
+    end
+
+    @ships = Ship.all
+    @ship1 = @ships[0]
+    @ship2 = @ships[1]
+    erb :show
+  end
+
+end
 end
